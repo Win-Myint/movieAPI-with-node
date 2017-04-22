616c21b334c71cce8659b0a4370709d3b3a5cc79
Win <win.m179@gmail.com>
2017-04-22 12:57:42 +0100

    added search.ejs, included a form which action is set to result url. User can now dynamically search for their fav movies

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ var express = require('express');
 var app     = express();
 var request = require('request');
 
+// set view engine to ejs
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-	request('http://www.omdbapi.com/?s=breaking', function(error, response, body) {
+// root route
+app.get('/', function(req, res){
+	res.render('search');
+})
+
+// result route
+app.get('/result', function(req, res){
+	// captured user search key words in "searchTerm" variable
+	var searchTerm = req.query.searchTerm;
+	// concat "searchTerm" with url to omdbAPI
+	var url = 'http://www.omdbapi.com/?s=' + searchTerm;
+	// pass "url" variable into "request" module
+	request(url, function(error, response, body) {
 		if(!error && response.statusCode == 200) {
 			var data = JSON.parse(body);
 			res.render("result", {data: data});
@@ -13,6 +25,7 @@ app.get('/', function(req, res) {
 	});
 });
 
-app.listen(3000, function() {
+// set up server to listen to port 3000
+app.listen(3000, function(){
 	console.log("listening to port 3000!");
 })
